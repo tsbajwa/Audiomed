e58a50690a3e865a6d893fe4961a7134fc6e6e1e
tsbajwa <teranjit@gmail.com>
2017-06-06 11:35:42 +1000

    Hello World working version

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,24 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+//import HtmlWebpackPlugin from 'html-webpack-plugin'
+
 
 module.exports = {
-
-    entry: {
-        main: './src/index.js'
-		},
-
-		output: {
-			path: __dirname + '/dist',
-			filename: "index_bundle.js"
-		},
-
-		module: {
-			rules: [
-				{
-					test: /\.js$/, 
-					exclude: /node_modules/, 
-					loader: "babel-loader"
-				},
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'css-loader']
-				}
-
-			]
-		},
-		plugins:[
+  entry: {
+    main: './src/index.js'
+	},
+  output: {
+    path: __dirname + '/src',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+			{test: /\.css$/,use: ['style-loader', 'css-loader']}
+    ]
+  },
+  plugins: [
 			new HtmlWebpackPlugin({template: __dirname + '/src/index.html', filename: 'index.html', inject: 'body'})
-		]
+	]
 }
+
