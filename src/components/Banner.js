38daa2bcf98c276bd0fa8efc75b712ecf1541007
tsbajwa <teranjit@gmail.com>
2017-06-08 16:26:52 +1000

    organizing styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 export default function Banner() {
   return (
-    <div className='container-banner'>
-    <div>
-        <img src= '../assets/logo-white.png'></img>
-    </div>
-    <div>
-        <img src='../assets/banner.png'></img>
-        <h1>Learning Anatomy made Simple</h1>
-        <h3>Stop wasting hours upon hours trying to learn Anatomy. There is an easier way.</h3>
-        <h4>
-        Audiomed is a learning system designed to teach you anatomy faster, saving you time and energy.
-        With over 250 anatomy mnemonic recordings, it is the smarter way to learn.
-        </h4>
-    </div>
+    <div className='banner'>
+      <div className='banner-logo'>
+          <img src= '../assets/logo-white.png'></img>
+      </div>
+      <div>
+          <img src='../assets/mobile-banner.png'></img>
+          <h1>Learning Anatomy made Simple</h1>
+          <h2>Stop wasting hours upon hours trying to learn Anatomy. There is an easier way.</h2>
+          <h4>
+          Audiomed is a learning system designed to teach you anatomy faster, saving you time and energy.
+          With over 250 anatomy mnemonic recordings, it is the smarter way to learn.
+          </h4>
+      </div>
     </div>
   );
 }
