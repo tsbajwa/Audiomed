f9c6d13c3258dc72f12ff5631b4bcf20073e0a0f
tsbajwa <teranjit@gmail.com>
2017-06-28 09:24:56 +1000

    Basic form component complete

--- src/components/Updating.js
+++ src/components/Updating.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
-export default function Updating() {
-  return (
-    <div>
-    MailChimp form integration goes here!
-    </div>
-  );
+class Updating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  handleSubmit(event) {
+    alert('Thanks, we will be in touch once the updated material launches');
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    );
+  }
 }
 
+module.exports = Updating;
